9076be057204a4c4e339f8b119a141fcf9938e7c
surendar4 <surendar1006@gmail.com>
2018-02-16 18:16:38 +0530

    testing

--- app/src/main/java/com/ssdevelopers/easyinterestcalculator/EasyInterestCalculator.kt
+++ app/src/main/java/com/ssdevelopers/easyinterestcalculator/EasyInterestCalculator.kt
@@ -15,7 +15,6 @@ class EasyInterestCalculator : AppCompatActivity(), NavigationView.OnNavigationI
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_easy_interest_calculator)
         setSupportActionBar(toolbar)
 
